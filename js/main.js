eb0abea1471474896d45e80298f1e2dd19e29ce0
c khomm <ckhomm2@gmail.com>
2023-09-21 11:57:55 +0700

    homepage

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
-function click_this(){
-    window.alert('This is technology website !')
+const hp_logo_category = document.querySelector(".hp_logo_category");
+const navHeight = 70;
+// the point the scroll starts from (in px)
+let lastScrollY = 0;
+// how far to scroll (in px) before triggering
+const delta = 10;
+
+// function to run on scrolling
+function scrolled() {
+  let sy = window.scrollY;
+  // only trigger if scrolled more than delta
+  if (Math.abs(lastScrollY - sy) > delta) {
+    // scroll down -> hide nav bar
+    if (sy > lastScrollY && sy > navHeight) {
+        hp_logo_category.classList.add("scroll-up");
+    } 
+    // scroll up -> show nav bar
+    else if (sy < lastScrollY) {
+        hp_logo_category.classList.remove("scroll-up");
+    }
+   // update current scroll point
+   lastScrollY = sy 
+  }
 }
+
+// Add event listener & debounce so not constantly checking for scroll
+let didScroll = false;
+window.addEventListener("scroll", function(e){
+  didScroll = true;
+});
+
+setInterval(function() {
+  if (didScroll) {
+    scrolled();
+    didScroll = false;
+   }
+}, 250)
