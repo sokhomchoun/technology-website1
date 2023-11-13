3dadc2e955ba33e88a0f12b2be6868c92b862208
c khomm <ckhomm2@gmail.com>
2023-11-13 23:43:39 +0700

    tech_news, detailstyle, see-more js

--- js/see-more.js
+++ js/see-more.js
@@ -15,6 +15,21 @@ function readmore() {
       moreText.style.display = "inline";
     }
   }
+  function readmore2() {
+    var dots = document.getElementById("dots2");
+    var moreText = document.getElementById("more2");
+    var btnText = document.getElementById("btn-readmore");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
 
 // view all btn
 
