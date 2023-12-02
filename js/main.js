3bfd18cf764125291b0fbdaa6934a91ab40dd6f1
khom <chounsokhom2002@gmail.com>
2023-12-03 01:06:52 +0700

    index, style, main

--- js/main.js
+++ js/main.js
@@ -27,6 +27,18 @@ $(document).ready(function(){
   });
 });
 
+$(document).ready(function(){
+  $('.hp_slide_info2').slick({
+  slidesToShow: 4,
+  slidestoScroll:1,
+  infinite: true,
+  // autoplay:true,
+  // dots:true,
+  centerMode: true,
+  prevArrow:'.hp_cheron_left_new',
+  nextArrow:'.hp_cheron_right_new',
+  });
+});
 // end slick slide scroll 
 
 // js link to display image 
@@ -47,6 +59,27 @@ const information3 = () => {
   document.getElementById("third").style.display ='block'
   document.getElementById("first").style.display ='none'
   document.getElementById("second").style.display ='none'
+}
+
+
+
+// js like recomment page
+const information4 = () => {
+  document.getElementById("four").style.display ='block';
+  document.getElementById("five").style.display ='none'
+  document.getElementById("six").style.display ='none'
+}
+
+const information5 = () => {
+  document.getElementById("five").style.display ='block'
+  document.getElementById("four").style.display ='none'
+  document.getElementById("six").style.display ='none'
+}
+
+const information6 = () => {
+  document.getElementById("six").style.display ='block' 
+  document.getElementById("four").style.display ='none'
+  document.getElementById("five").style.display ='none'
 }
 
 // end js link to display image 
@@ -119,14 +152,67 @@ window.onclick = function(event) {
   }
 }
 
+  // click show three dot
+
+function showDropdown5() {
+  document.getElementById("hp_dropdown_list5").classList.toggle("show5");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show5')) {
+              openDropdown.classList.remove('show5');
+          }
+      } 
+  }
+}
+
+function showDropdown6() {
+  document.getElementById("hp_dropdown_list6").classList.toggle("show6");
+
+}
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show6')) {
+              openDropdown.classList.remove('show6');
+          }
+      }
+  }
+}
+function showDropdown7() {
+  document.getElementById("hp_dropdown_list7").classList.toggle("show7");
+
+}
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show7')) {
+              openDropdown.classList.remove('show7');
+          }
+      }
+  }
+}
+
+
 // end js dot three
 
 
 // progress scroll header 
 const progressContainerEl = document.querySelector(".post-container");
 const progressBarEl = document.querySelector(".progress-bar-container__progress");
-
-// function to check scroll position and update scroll progress bar accordingly
 const updateScrollProgressBar = () => {
   // get full scroll height
   const scrollHeight = progressContainerEl.scrollHeight - heightInViewport(progressContainerEl);
@@ -143,7 +229,6 @@ const updateScrollProgressBar = () => {
 progressContainerEl.addEventListener("scroll", updateScrollProgressBar)
 progressContainerEl.addEventListener("load", updateScrollProgressBar)
 
-
 function heightInViewport(el) {
     var elH = el.offsetHeight,
         H   = document.body.offsetHeight,
@@ -152,6 +237,35 @@ function heightInViewport(el) {
 }
 
 // end progress scroll header 
+
+// Reverted back to non-arrow functions because it would break IE11 completely.
+
+var observer = new IntersectionObserver(
+	function (entries) {
+		entries.forEach(function (entry) {
+			var el = entry.target;
+			if (entry.isIntersecting) {
+				el.classList.add("animate");
+				return;
+			}
+		});
+	},
+	{ threshold: 0.2 }
+);
+
+document.querySelectorAll(".animation").forEach(function (i) {
+	if (i) {
+		observer.observe(i);
+	}
+});
+
+
+
+
+
+
+
+
 
 
 
