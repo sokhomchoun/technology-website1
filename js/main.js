b542d158ce6613dcfb0f3919ed0016724fe743a8
khom <chounsokhom2002@gmail.com>
2023-12-12 13:28:50 +0700

    top page

--- js/main.js
+++ js/main.js
@@ -25,20 +25,41 @@ $(document).ready(function(){
 
 const information1 = () => {
   document.getElementById("first").style.display ='block';
-  document.getElementById("second").style.display ='none'
-  document.getElementById("third").style.display ='none'
+  document.getElementById("second").style.display ='none';
+  document.getElementById("third").style.display ='none';
+  document.getElementById("third1").style.display ='none';
+  document.getElementById("third2").style.display ='none';
 }
 
 const information2 = () => {
-  document.getElementById("second").style.display ='block'
-  document.getElementById("first").style.display ='none'
-  document.getElementById("third").style.display ='none'
+  document.getElementById("second").style.display ='block';
+  document.getElementById("first").style.display ='none';
+  document.getElementById("third").style.display ='none';
+  document.getElementById("third1").style.display ='none';
+  document.getElementById("third2").style.display ='none';
 }
 
 const information3 = () => {
-  document.getElementById("third").style.display ='block'
-  document.getElementById("first").style.display ='none'
-  document.getElementById("second").style.display ='none'
+  document.getElementById("third").style.display ='block';
+  document.getElementById("third1").style.display ='none';
+  document.getElementById("third2").style.display ='none';
+  document.getElementById("first").style.display ='none';
+  document.getElementById("second").style.display ='none';
+}
+
+const information33 = () => {
+  document.getElementById("third1").style.display ='block';
+  document.getElementById("third2").style.display ='none';
+  document.getElementById("third").style.display ='none';
+  document.getElementById("first").style.display ='none';
+  document.getElementById("second").style.display ='none';
+}
+const information34 = () => {
+  document.getElementById("third2").style.display ='block';
+  document.getElementById("third1").style.display ='none';
+  document.getElementById("third").style.display ='none';
+  document.getElementById("first").style.display ='none';
+  document.getElementById("second").style.display ='none';
 }
 
 
@@ -46,21 +67,47 @@ const information3 = () => {
 // js like recomment page
 const information4 = () => {
   document.getElementById("four").style.display ='block';
-  document.getElementById("five").style.display ='none'
-  document.getElementById("six").style.display ='none'
+  document.getElementById("five").style.display ='none';
+  document.getElementById("six").style.display ='none';
+  document.getElementById("seven").style.display ='none';
+  document.getElementById("eight").style.display ='none';
 }
 
 const information5 = () => {
-  document.getElementById("five").style.display ='block'
-  document.getElementById("four").style.display ='none'
-  document.getElementById("six").style.display ='none'
+  document.getElementById("five").style.display ='block';
+  document.getElementById("four").style.display ='none';
+  document.getElementById("six").style.display ='none';
+  document.getElementById("seven").style.display ='none';
+  document.getElementById("eight").style.display ='none';
 }
 
 const information6 = () => {
-  document.getElementById("six").style.display ='block' 
-  document.getElementById("four").style.display ='none'
-  document.getElementById("five").style.display ='none'
+  document.getElementById("six").style.display ='block' ;
+  document.getElementById("seven").style.display ='none';
+  document.getElementById("eight").style.display ='none';
+  document.getElementById("four").style.display ='none';
+  document.getElementById("five").style.display ='none';
 }
+const information7 = () => {
+  document.getElementById("seven").style.display ='block';
+  document.getElementById("eight").style.display ='none';
+  document.getElementById("six").style.display ='none';
+  document.getElementById("four").style.display ='none';
+  document.getElementById("five").style.display ='none';
+}
+const information8 = () => {
+  document.getElementById("eight").style.display ='block';
+  document.getElementById("seven").style.display ='none';
+  document.getElementById("six").style.display ='none';
+  document.getElementById("four").style.display ='none';
+  document.getElementById("five").style.display ='none';
+}
+
+
+
+
+
+
 
 // end js link to display image 
 
